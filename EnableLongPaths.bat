26bb6c58632f84864324ef5a8405c7314e69d4bb
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2025-12-02 20:23:01 +0900

    再起動のメッセージ追加。

--- EnableLongPaths.bat
+++ EnableLongPaths.bat
@@ -7,7 +7,7 @@ if %ERRORLEVEL% equ 0 ( exit /b 0 )
 echo reg add "HKLM\SYSTEM\CurrentControlSet\Control\FileSystem" /v LongPathsEnabled /t REG_DWORD /d 1 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\FileSystem" /v LongPathsEnabled /t REG_DWORD /d 1 /f
 if %ERRORLEVEL% neq 0 (
-	echo "EasyEnv/EnableLongPaths.bat を右クリックして、「管理者として実行」してください。"
-	echo "Right-click EasyEnv/EnableLongPaths.bat and select 'Run as administrator'."
+	echo "EasyEnv/EnableLongPaths.bat を右クリックして、「管理者として実行」して再起動します。"
+	echo "Right-click EasyEnv/EnableLongPaths.bat and select 'Run as administrator', then restart."
 	pause & exit /b 1
 )
